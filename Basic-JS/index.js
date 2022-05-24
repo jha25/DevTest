ee3a08af349b77689e03ec022656998ece39e03d
jha25 <jha25@wisc.edu>
2022-05-24 11:44:17 -0500

    changes

--- Basic-JS/index.js
+++ Basic-JS/index.js
@@ -1,31 +1,80 @@
-// Developer Test 
+/** @format */
+
+// Developer Test
 
 // Basic JavaScript (bonus for efficiency):
 
 // Below are varriables to be used for the below questions
 // Global Varriables
 
-var departmentInfo = require('./json/departmentInfo.json');
-var objectArray = require('./json/objectArray.json');
-var people = require('./json/people.json');
+var departmentInfo = require("./json/departmentInfo.json")
+var objectArray = require("./json/objectArray.json")
+var people = require("./json/people.json")
 
 var objectF = {
-  value: 'f',
-  Id: 5
-};
+	value: "f",
+	Id: 5,
+}
 
-
-// end Global Varriables 
+// end Global Varriables
 
 // 1. Write a function that adds two given numbers
 
+const add = (num1, num2) => {
+	// Validating inputs
+	if (typeof num1 !== "number" || typeof num2 !== "number")
+		return "Both input should be numbers"
+
+	return num1 + num2
+}
+
+// console.log(add(2, 3))
 
 // 2. Adding 'objectF' to an 'objectArray'
 
+const updateObjectArray = (object) => {
+	// Validating input
+	if (typeof object !== "object") return "Input should be an object"
+
+	objectArray[objectArray.length] = object
+	return objectArray
+}
+
+// updateObjectArray(objectF)
 
 // 3. Write a function that returns Id 3 from 'objectArray'
 
+const getId = (objectArray) => {
+	// Validate inputs
+	if (typeof objectArray !== "object") return "Input should be an object"
+
+	return objectArray.find((object) => object.Id === 3)
+}
+// getId(objectArray)
 
 // 4. Write a function that merges 'people' with 'departmentInfo' based on department
 
+const addFacultyToDepartment = (department, faculty) => {
+	// Valid Input
+	if (!Array.isArray(department) || !Array.isArray(faculty))
+		return "Input should be an array"
 
+	// Create map to record visited keys && values
+	let departmentMap = new Map()
+
+	for (let i = 0; i < department.length; i++) {
+		const departmentObj = department[i]
+		console.log(departmentObj)
+		// Nested departmentInfo within values of "department" to reduce time complexity
+		// create faculty array to insert people data
+		departmentMap[departmentObj.department] = { ...departmentObj, faculty: [] }
+	}
+
+	for (let j = 0; j < people.length; j++) {
+		const peopleObj = people[j]
+		// Push people's file data within correct department
+		departmentMap[peopleObj.department].faculty.push(peopleObj)
+	}
+}
+
+addFacultyToDepartment(departmentInfo, people)
--- Basic-RxJs/index.js
+++ Basic-RxJs/index.js
@@ -1,27 +1,44 @@
-const { of } = require("rxjs");
-const { map, filter } = require("rxjs/operators");
+/** @format */
+
+const { of } = require("rxjs")
+const { map, filter, tap } = require("rxjs/operators")
 
 // - Pipe operators (map, tap, switchMap)
 
+of([
+	{
+		id: 1,
+		name: "Fabian",
+	},
+	{ id: 2, name: "Jan-Niklas" },
+])
+	.pipe(tap((item) => (item.name = item.name + "_2")))
+	.subscribe((x) => console.log(x))
 
 // - what will be logged to the console
 
-of('Hello').pipe(
-  map(x => `${x} World`),
-  map(x => `${x} of`),
-  map(x => `${x} RxJS`),
-).subscribe(x => {
-  console.log(x);
-})
+of("Hello")
+	.pipe(
+		map((x) => `${x} World`),
+		map((x) => `${x} of`),
+		map((x) => `${x} RxJS`),
+	)
+	.subscribe((x) => {
+		console.log(x)
+	})
 
-// - What will be logged to the console 
-of(1, 2, 3).pipe(
-  map(x => x + 1),
-  filter(x => x > 2)
-).subscribe(x => {
-  console.log(x);
-})
+// Log: "Hello World of RxJS" as it is implementing the previous string into the template string
 
-// - Subscription sequence 
+// - What will be logged to the console
+of(1, 2, 3)
+	.pipe(
+		map((x) => x + 1),
+		filter((x) => x > 2),
+	)
+	.subscribe((x) => {
+		console.log(x)
+	})
 
+// each element is incremented by 1 and then the elements that are greater than 2 are kept. Log: 3 and 4
 
+// - Subscription sequence
